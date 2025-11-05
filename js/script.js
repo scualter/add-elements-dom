2d7ab22561de2a92d4367322cd52c75f7e3f6bff
Ruben <rubencarreteropelaez@gmail.com>
2025-11-06 00:22:25 +0100

    Añadido CSS y modificaciones en index.html y script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
  // Aquí tu código
+const boton = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+boton.addEventListener('click', function() {
+    const elemento = prompt('Introduce un elemento para la lista:');
+
+    if(elemento) {
+        const li = document.createElement('li');
+        li.textContent = elemento;
+
+        const btnEliminar = document.createElement('button');
+        btnEliminar.textContent = 'Eliminar';
+        btnEliminar.className = 'eliminar';
+
+        btnEliminar.addEventListener('click', function() {
+            lista.removeChild(li);
+        });
+
+        li.appendChild(btnEliminar);
+        lista.appendChild(li);
+    }
+});
